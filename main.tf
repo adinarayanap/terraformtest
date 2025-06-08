c0a718d8f6b26d0a4e4a1305f4b61d2e68158808
Adi <adipolisetty@outlook.com>
2025-06-08 22:01:11 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,24 +1,37 @@
-#Terraform
+# Configure the Google Cloud provider
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "6.8.0"
+      source  = "hashicorp/google"
+      version = "~> 4.0"
     }
   }
 }
 
+# Configure the Google Cloud provider
 provider "google" {
   project = "testadi-459816"
   region  = "us-central1"
-  zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-<<<<<<< HEAD
+# Create a Compute Engine VM instance
+resource "google_compute_instance" "default" {
+  name         = "my-vm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  # Boot disk configuration
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  # Network interface configuration
+  network_interface {
+    network = "default" # Use the default VPC network
+    access_config {
+      # Enable public access to the VM
+    }
+  }
 }
-=======
-  #added the new line
-}
->>>>>>> abf0466a1c4b6d2fa72e79e1e813db7d2545c59c
